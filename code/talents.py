5bd25fc8677fcee7ad604e3559a751886ceb3c6b
Petr Kmoch <petr.kmoch@gmail.com>
2021-03-19 15:23:33 +0100

    WIP on arguments

---
+++ code/talents.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2021 Petr Kmoch
+#
+# Distributed under the MIT license, see accompanying LICENSE file
+
+import argparse
+import sys
+import xml.etree.ElementTree as ET
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        prog=argv[0],
+        description="E&P talent evaluator",
+    )
+    # parser.add_argument(
+        # "-i", "--input",
+        # help="",
+    # )
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv)
